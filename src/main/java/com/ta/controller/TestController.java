237acac061f2e3db3a5c09a79b320d552df33e15
rvsusee <rvsusee@gmail.com>
2022-11-08 18:23:23 +0530

    08-Nov-2022

--- src/main/java/com/ta/controller/TestController.java
+++ src/main/java/com/ta/controller/TestController.java
@@ -5,6 +5,7 @@ import java.util.InputMismatchException;
 import java.util.Map;
 import java.util.Properties;
 
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
@@ -28,52 +29,54 @@ public class TestController {
 	@Value("${filepath}")
 	String path;
 
-	@RequestMapping(value = "testapi1", method = RequestMethod.GET)
-	public ResponseEntity<?> testAPI(@RequestParam Map<String, String> inputs) throws Exception {
+	Logger log = org.apache.logging.log4j.LogManager.getLogger("TestController");
 
+	@RequestMapping(value = "inputs", method = RequestMethod.GET)
+	public ResponseEntity<?> testAPI(@RequestParam Map<String, String> inputs) throws Exception {
+		System.out.println("input service");
+		log.info("inputs service started: ");
+		log.trace("inputs controller started");
+		log.debug("testAPI started: " + inputs);
 		try {
 			String operation = inputs.get("operation");
+			log.warn("Input type casting ");
 			int v1 = Integer.parseInt(inputs.get("a"));
 			int v2 = Integer.parseInt(inputs.get("b"));
-			int result = 0;
-
-			if ("add".equals(operation)) {
-				result = calc.add(v1, v2);
-			} else if ("sub".equals(operation)) {
-				result = calc.sub(v1, v2);
-			} else if ("mul".equals(operation)) {
-				result = calc.mul(v1, v2);
-			} else if ("div".equals(operation)) {
-				result = calc.div(v1, v2);
-			} else {
-				throw new InputMismatchException("Invalid Operation");
-			}
-			
-			
-			inputs.put("Result", result + "");
+			log.debug("Values: a - " + v1 + " and b - " + v2);
 			inputs.put("Status", "Success");
+			throw new InputMismatchException("Invalid Operation");
 		} catch (Exception e) {
+			log.error("An ERROR Message");
 			inputs.put("Status", "Failure");
 			inputs.put("Error", e.toString());
 		}
-		
-		
-		return new ResponseEntity<>(inputs, HttpStatus.OK);
 
+		log.info("Input Values: " + inputs);
+		log.debug("inputs controller  ended: " + inputs);
+		log.trace("inputs controller ended");
+		System.out.println("testAPI Ended");
+		return new ResponseEntity<>(inputs, HttpStatus.OK);
 	}
 
-	
-	@RequestMapping(value = "testapi2", method = RequestMethod.GET)
+	@RequestMapping(value = "readFile", method = RequestMethod.GET)
 	public ResponseEntity<?> testAPI2() {
-
+		log.trace("inputs controller started");
+		log.info("inputs controller started");
 		Properties properties = null;
 		try {
 			FileInputStream file = new FileInputStream(path);
 			properties = new Properties();
 			properties.load(file);
+			log.info("properties Values : "+properties);
 		} catch (Exception e) {
+			log.error("An ERROR Message: "+e);
 			System.out.println(e);
 		}
+		log.info("Input Values: " + properties);
+		log.debug("inputs controller ended: " + properties);
+		log.trace("inputs controller ended");
+		System.out.println("testAPI Ended");
+		
 		return new ResponseEntity<>(properties, HttpStatus.OK);
 
 	}
--- src/main/java/com/ta/controller/UserController.java
+++ src/main/java/com/ta/controller/UserController.java
@@ -2,6 +2,8 @@ package com.ta.controller;
 
 import java.util.Map;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,17 +20,35 @@ public class UserController {
 	@Autowired
 	User user;
 
+	Logger log = LogManager.getLogger("UserController");
+	
 	@RequestMapping(value = "login", method = RequestMethod.GET)
 	public String loginUser(@RequestParam Map<String, String> input) throws Exception {
+		log.info("Login Service started: " + input);
+
 		if (user.checkUserPass(input.get("name"), input.get("pass"))) {
+			log.debug("Checking : " + "Login Success");
+			log.info("Login Service Ended: " + "Login Success");
 			return "Login Success";
 		}
+		
+		log.debug("Login Failed: " + input);
+		log.info("Login Service Ended: " + input);
 		return "Login Failed";
+
 	}
 
+	
 	@GetMapping(value = "getusers")
 	public String getUsers() throws Exception {
-		return user.toString();
+		log.info("getusers Service started ");
+		log.debug("getusers service started: ");
+		String userValue = user.toString();
+
+		log.debug("getusers service Values: "+userValue);
+
+		log.debug("getusers service Ended: ");
+		return userValue;
 	}
 
 }
